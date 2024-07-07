a0a3a1cdf7c09d067f38a9fdac47a4faa1a89556
Gerfy <gerfynlol@gmail.com>
2024-07-07 16:40:23 -0300

    fix & add: infos spring doc

--- src/main/java/com/forum/project/ForumProject/infra/security/SecurityConfigurations.java
+++ src/main/java/com/forum/project/ForumProject/infra/security/SecurityConfigurations.java
@@ -1,6 +1,12 @@
 package com.forum.project.ForumProject.infra.security;
 
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -44,5 +50,25 @@ public class SecurityConfigurations {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("API Forum HUB")
+                        .description("API Rest do FORUM HUB, contendo as funcionalidades de CRUD de usuarios, perfil, topico, respostas e validações")
+                        .contact(new Contact()
+                                .name("Time Backend")
+                                .email("backend@forum.hub"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://forum.hub/api/licenca")));
     }
 }
